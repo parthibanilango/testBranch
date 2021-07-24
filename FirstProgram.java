607a42d692048ed9a230b1f3d502b960251bfd97
parthibanilango <parthibanilango89@gmail.com>
2021-07-24 18:20:42 +0530

    test git integration

---
+++ FirstProgram.java
@@ -0,0 +1,19 @@
+public class FirstProgram {
+
+        // define main method
+		public static void main(String args[]){
+			System.out.println("My first Program in Java");
+		int grade ='B';
+
+    System.out.println(grade);
+	    int no = 10;
+
+                     String res = 10%2==0?"even":"odd";
+
+                     System.out.println(res);
+		}
+		
+
+        // Print the output
+     
+}
